56a5037683f26ed8ae6729578559cb0e509fab43
alee1414 <luisribota1@gmail.com>
2025-11-14 12:18:21 -0600

    Test ResponseTraits Camiones

--- app/Controllers/Camiones.php
+++ app/Controllers/Camiones.php
@@ -37,6 +37,13 @@ class Camiones extends ResourceController
 
     public function update($id = null)
     {
+        // Test inicia
+        $response = $this->setResponseToSend(200, ["id" => 1, "nombre" => "Carlos"], null);
+        //$response = $this->setResponseToSend(404, null, "Fallo al intentar hacer algo");
+        return $this->respond($response);
+        exit;
+        // Test termina
+
         $data = $this->request->getRawInput();
         $this->model->update($id, $data);
         return $this->respond($data);
@@ -46,6 +53,24 @@ class Camiones extends ResourceController
     {
         $this->model->delete($id);
         return $this->respondDeleted(['id' => $id]);
+    }
+
+    private function setResponseToSend($code, $data, $message)
+    {
+        if($code == 200){
+
+            $response = [
+            "success" => true,
+            "data" => $data
+            ];   
+        }
+        if($code == 404){
+            $response = [
+            "success" => false,
+            "message" => $message
+            ]; 
+        }
+        return $response;
     }
 
 }
