8df85b50d3045a3acd07e83a616ea579932df0b0
alee1414 <luisribota1@gmail.com>
2025-11-19 09:07:59 -0600

    routes de los controladores

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,4 +7,12 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->resource('camiones');
-
+$routes->resource('choferes');
+$routes->resource('estaciones');
+$routes->resource('historialpago');
+$routes->resource('rutaestaciones');
+$routes->resource('rutas');
+$routes->resource('tarifas');
+$routes->resource('ubicacionescamiones');
+$routes->resource('usuario');
+$routes->resource('viajes');
